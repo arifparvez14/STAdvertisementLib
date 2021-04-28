fc2bb64d8c2bc0e1e06e7927a6f89eb752210f6c
Muhammad Ariful Islam <arif.mbstu22@gmail.com>
2021-04-28 12:11:16 +0600

    test added

---
+++ STAdvertisementLib/STLib.swift
@@ -0,0 +1,22 @@
+//
+//  STLib.swift
+//  STAdvertisementLib
+//
+//  Created by ShareTrip iOS on 28/4/21.
+//
+
+import Foundation
+
+public final class SwiftyLib {
+
+    let name = "SwiftyLib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
